e90166f3179048219a6ea1f90eaae75c6466e46d
MiguelDeLunaPonce2007 <64348087+MiguelDeLunaPonce2007@users.noreply.github.com>
2020-04-26 14:55:30 -0500

    Add files via upload

---
+++ mi tercer programa mike.c
@@ -0,0 +1,16 @@
+//The name of the members of the family
+#include<stdio.h>
+#include<stdio.h>
+
+int main ()
+{
+    printf("The name of the members of the family:\t");
+    printf("Juan De Luna ponce.:\t");
+    printf("Rosa Imelda Ponce.:\t");
+    printf("Juan De Luna Ponce.:\t");
+    printf("Maria Elena De Luna Ponce.:\t");
+    printf("Mariana Isabel De Luna Ponce.:\t");
+    printf("Miguel Angel De Luna Ponce.:\t");
+
+    return 0;
+}
